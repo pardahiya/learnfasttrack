0bf459b1b3aabf7e7bcb0f89c440931603d47fab
Parvesh Dahiya <parveshdahiya@email.com>
2023-05-25 22:41:54 +1000

    more changes

--- assets/js/dropdown.js
+++ assets/js/dropdown.js
@@ -2,99 +2,101 @@
 // dropdown.js
 //
 
-'use strict';
+"use strict";
 
-(function() {
+(function () {
+   //
+   // Variables
+   //
 
-  //
-  // Variables
-  //
+   // Selectors
+   var drops = document.querySelectorAll(
+      ".navbar-nav .dropdown, .navbar-nav .dropright"
+   );
 
-  // Selectors
-  var drops = document.querySelectorAll('.navbar-nav .dropdown, .navbar-nav .dropright');
+   // Events
+   var showEvents = ["mouseenter"];
+   var hideEvents = ["mouseleave", "click"];
 
-  // Events
-  var showEvents = ['mouseenter'];
-  var hideEvents = ['mouseleave', 'click'];
+   // Transition
+   var transitionDuration = 200;
 
-  // Transition
-  var transitionDuration = 200;
+   // Breakpoint
+   var desktopSize = 992;
 
-  // Breakpoint
-  var desktopSize = 992;
+   //
+   // Functions
+   //
 
-  //
-  // Functions
-  //
-
-  // Show drop
-  function showDrop(menu) {
-    if (window.innerWidth < desktopSize) {
-      return;
-    }
-
-    menu.classList.add('showing');
-
-    setTimeout(function() {
-      menu.classList.remove('showing');
-      menu.classList.add('show');
-    }, 1);
-  }
-
-  // Hide drop
-  function hideDrop(e, menu) {
-    if (window.innerWidth < desktopSize) {
-      return;
-    }
-
-    if (!menu.classList.contains('show')) {
-      return;
-    }
-
-    if (e.type === 'click' && e.target.closest('.dropdown-menu form')) {
-      return;
-    }
-
-    menu.classList.add('showing');
-    menu.classList.remove('show');
-
-    setTimeout(function() {
-      menu.classList.remove('showing');
-    }, transitionDuration);
-  }
-
-  //
-  // Listeners
-  //
-
-  [].forEach.call(drops, function(dropdown) {
-    var menu = dropdown.querySelector('.dropdown-menu');
-
-    // Show drop
-    showEvents.forEach(function(event) {
-      dropdown.addEventListener(event, function() {
-        showDrop(menu);
-      });
-    });
-
-    // Hide drop
-    hideEvents.forEach(function(event) {
-      dropdown.addEventListener(event, function(e) {
-        hideDrop(e, menu);
-      });
-    });
-  });
-
-  $('.quantity-plus, .quantity-minus').on('click', function(e) {
-      var isNegative = $(e.target).closest('.quantity-minus').is('.quantity-minus');
-      var input = $(e.target).closest('.quantity-increment').find('input');
-      if (input.is('input')) {
-          if (isNegative) {
-              input[0].stepDown();
-          } else {
-              input[0].stepUp();
-          }
+   // Show drop
+   function showDrop(menu) {
+      if (window.innerWidth < desktopSize) {
+         return;
       }
-  })
 
+      menu.classList.add("showing");
+
+      setTimeout(function () {
+         menu.classList.remove("showing");
+         menu.classList.add("show");
+      }, 1);
+   }
+
+   // Hide drop
+   function hideDrop(e, menu) {
+      if (window.innerWidth < desktopSize) {
+         return;
+      }
+
+      if (!menu.classList.contains("show")) {
+         return;
+      }
+
+      if (e.type === "click" && e.target.closest(".dropdown-menu form")) {
+         return;
+      }
+
+      menu.classList.add("showing");
+      menu.classList.remove("show");
+
+      setTimeout(function () {
+         menu.classList.remove("showing");
+      }, transitionDuration);
+   }
+
+   //
+   // Listeners
+   //
+
+   [].forEach.call(drops, function (dropdown) {
+      var menu = dropdown.querySelector(".dropdown-menu");
+
+      // Show drop
+      showEvents.forEach(function (event) {
+         dropdown.addEventListener(event, function () {
+            showDrop(menu);
+         });
+      });
+
+      // Hide drop
+      hideEvents.forEach(function (event) {
+         dropdown.addEventListener(event, function (e) {
+            hideDrop(e, menu);
+         });
+      });
+   });
+
+   $(".quantity-plus, .quantity-minus").on("click", function (e) {
+      var isNegative = $(e.target)
+         .closest(".quantity-minus")
+         .is(".quantity-minus");
+      var input = $(e.target).closest(".quantity-increment").find("input");
+      if (input.is("input")) {
+         if (isNegative) {
+            input[0].stepDown();
+         } else {
+            input[0].stepUp();
+         }
+      }
+   });
 })();
